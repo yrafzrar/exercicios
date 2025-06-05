a1ae5ae4b78971241faf1224124d37cee75be4fc
yrafzrar <rafarl.areis@outlook.com>
2025-06-05 08:21:39 -0300

    Create ex4.py

---
+++ pasta/lista1/ex4.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite sua primeira nota do primeiro semestre\n"))
+n2 = float(input("Digite sua segunda nota do primeiro semestre\n"))
+n3 = float(input("Agora digite a primeira nota do segundo semestre\n"))
+n4 = float(input("Por último, a segunda nota do segundo semestre \n"))
+final = (n1 + n2 + n3 + n4)/4
+print(final)
