168cc1c15fa38e90ea411bbc212386c396615ea8
yrafzrar <rafarl.areis@outlook.com>
2025-06-06 07:39:48 -0300

    Update ex1.py

--- pasta/lista2/ex1.py
+++ pasta/lista2/ex1.py
@@ -1,1 +1,8 @@
-
+numero = int(input("Digite um número inteiro: \n"))
+#print(numero/2)
+#print(numero//2)
+print(numero%2)
+if numero%2 == 0:
+    print("O número ",numero ," é par")
+else:
+    print("O número", numero, "é impar")
